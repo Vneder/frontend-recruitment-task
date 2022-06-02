e064dffd95551d2308184d1bbb883b87524a614b
Vneder <4007mj@gmail.com>
2022-06-03 00:47:46 +0200

    Second commit - add resetBtn, popup and css

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,25 @@
+const popup = document.querySelector(".popup");
 const eventButton = document.querySelector(".eventButton");
+const resetButton = document.querySelector(".resetButton");
 let countClickButton = 0;
 //localStorage dodac
 
+function resetCount() {
+    countClickButton = 0;
+    resetButton.style.visibility = "hidden";
+    document.querySelector(".counting").innerText = countClickButton + " times";
+}
 
 function Popup() {
     countClickButton++;
+
+    //popup.classList.toggle("active");
+
     document.querySelector(".counting").innerText = countClickButton + " times";
     if (countClickButton > 5) {
-        alert("To już ponad 5");
-        // Pokazać przycisk w popupie do resetowania licznika
+        resetButton.style.visibility = "visible";
     }
 }
 
 eventButton.addEventListener("click", Popup);
+resetButton.addEventListener("click", resetCount);
