ba0760f6fd233501c4c57ea25141917b8ace1592
Vneder <4007mj@gmail.com>
2022-06-03 15:31:13 +0200

    Add action on btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 const popup = document.querySelector(".popup");
+const content = document.querySelector(".content");
 const eventButton = document.querySelector(".eventButton");
 const resetButton = document.querySelector(".resetButton");
+const closeButton = document.querySelector(".closeButton");
 let countClickButton = 0;
 //localStorage dodac
+
+function closePopup() {
+    popup.classList.remove("active");
+    content.classList.remove("active");
+}
 
 function resetCount() {
     countClickButton = 0;
@@ -13,7 +20,8 @@ function resetCount() {
 function Popup() {
     countClickButton++;
 
-    //popup.classList.toggle("active");
+    popup.classList.add("active");
+    content.classList.add("active");
 
     document.querySelector(".counting").innerText = countClickButton + " times";
     if (countClickButton > 5) {
@@ -23,3 +31,4 @@ function Popup() {
 
 eventButton.addEventListener("click", Popup);
 resetButton.addEventListener("click", resetCount);
+closeButton.addEventListener("click", closePopup);
